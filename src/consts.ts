90d07d8475c0944db0b03a35230f6a72638a7475
Raj <rajesh-pandey@outlook.in>
2026-06-24 00:42:05 +0530

    feat: added portfolio site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Rajesh Pandey';
+export const SITE_DESCRIPTION =
+	'Lead Technical Engineer building cloud architecture, backend services, and mobile apps for healthcare platforms.';
